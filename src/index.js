4abf7524b1ecbffc38b1efb704c4a4cee7e39c2f
alexmeirelles <alemeirelles@msn.com>
2022-05-10 23:50:24 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Home from "./views/Home/Home";
 import "assets/styles/main.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,3 +9,23 @@ root.render(
     <Home />
   </React.StrictMode>,
 );
+
+// esse codigo acima adiciona a paleta, mas necessita atualizar manualmente para ver //
+
+//o codigo abaixo adiciona a paleta, atualiza automaticamente mostrando 3, qdo da o f5, aparece apenas uma que foi adicionada// 
+
+// esta ocorrendo problema de versão de react //
+
+/* 
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./views/Home/Home"
+import './assets/styles/main.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+*/
